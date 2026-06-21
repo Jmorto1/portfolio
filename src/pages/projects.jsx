452756738394880bff5c211067eb45dd45679125
yealem birhanu <yealem082@gmail.com>
2026-06-21 23:47:18 +0300

    adding two projects

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,7 +1,8 @@
 import "../styles/projects.css"
 import { useState,useEffect,useRef} from "react";
-import { FaReact} from "react-icons/fa";
-import { SiTypescript, SiDjango } from "react-icons/si";
+import { FaReact, FaNodeJs } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
+import { SiTypescript, SiDjango, SiFlutter } from "react-icons/si";
 import { DiJava } from "react-icons/di";
 
 export default function Project(){
@@ -27,7 +28,6 @@ export default function Project(){
         img:"images/projects/ethio_calendar.png",
         name:"Ethiopian Calendar",
         desc:"Ethiopian Calendar input field for React applications",
-        
         liveLink:"https://ethio-calendar.vercel.app/",
         sourceLink:"https://github.com/Jmorto1/ethio-calendar",
         tech:["React","Node.js"],
@@ -37,16 +37,40 @@ export default function Project(){
             { name: "TypeScript", icon: SiTypescript}
         ]
     },{id:"3",
+        img:"images/projects/map.png",
+        name:"Medicine Availability Platform",
+        desc:"A platform to check the availability of medicines in nearby pharmacies",
+        liveLink:"https://map-fp.vercel.app/",
+        sourceLink:"https://github.com/redi-b/map",
+        tech:["NextJs","NodeJs"],
+        longDesc:"A platform that lets users search for medicine availability at nearby pharmacies, upload prescriptions, set medicine adherence reminders, and consult an AI chatbot for medicine-related questions.",
+            techTags: [
+            { name: "NextJs", icon: SiNextdotjs },
+            {name:"NodeJs", icon: FaNodeJs}
+        ]
+    },{id:"4",
+        img:"images/projects/airline.png",
+        name:"AirSync",
+        desc:"A mobile app for airline passengers to track flight status and gate information",
+        sourceLink:"https://github.com/redi-b/airsync",
+        tech:["Flutter"],
+        longDesc:"AirSync is a mobile app for airline passengers built with Flutter which offers features such as flight status tracking, gate information, and in-app notifications, all while maintaining a visually appealing and user-friendly interface. It combines modern design features like glassmorphism, gradients, and micro-animations with a real-time Firebase Firestore backend to provide a smooth travel experience.",
+            techTags: [
+            { name: "Flutter", icon: SiFlutter},
+        ]
+    },
+    {id:"5",
         img:"images/projects/pprs.png",
         name:"Preemitive Priority Scheduling Simulator",
         desc:"Simulator for Preemptive Priority Scheduling Algorithm in Operating Systems",
         sourceLink:"https://github.com/Jmorto1/preemptive-priority-scheduling-GUI",
-        tech:["React","Node.js"],
+        tech:["Java","Swing"],
         longDesc:"This project is a simulator for the Preemptive Priority Scheduling Algorithm used in operating systems. It allows users to visualize how processes are scheduled based on their priority levels. The simulator provides an interactive interface for users to input process details, such as arrival time, burst time, and priority, and then observe the scheduling behavior through graphical representations and performance metrics.",
         techTags: [
             { name: "Java", icon: DiJava }
         ]
-    }]
+    }
+]
     useEffect(()=>{
         const handleClickOutside=(e)=>{
             if(!selectedProject) return;
